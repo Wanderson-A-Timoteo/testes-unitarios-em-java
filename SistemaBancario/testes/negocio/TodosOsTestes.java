55fcc2adfcf2b7e91daeb26d6d447f1f786c286d
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-01-27 18:53:39 -0300

    Add exemplo de calculos financeiros e limite de valores com multiplos cenarios

--- SistemaBancario/testes/negocio/TodosOsTestes.java
+++ SistemaBancario/testes/negocio/TodosOsTestes.java
@@ -9,7 +9,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	GerenciadoraClientesTest_Ex3.class, GerenciadoraClientesTest_Ex4.class, GerenciadoraClientesTest_Ex5.class,
 	GerenciadoraClientesTest_Ex7.class, GerenciadoraClientesTest_Ex8.class,	GerenciadoraContasTest_Ex3.class, 
 	GerenciadoraContasTest_Ex4.class, GerenciadoraContasTest_Ex6.class,
-	GerenciadoraClientesTest_Ex10.class})
+	GerenciadoraClientesTest_Ex10.class, GerenciadoraContasTest_Ex11.class})
 public class TodosOsTestes {
 
 }
