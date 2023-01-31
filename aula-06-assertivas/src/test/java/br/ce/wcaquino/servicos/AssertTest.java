d66df45bb693abc9d19df408e913bd3dccf78c6d
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-01-31 14:58:04 -0300

    Add exemplo Erro ao comparar dois objetos iguais com assertEquals

--- aula-06-assertivas/src/test/java/br/ce/wcaquino/servicos/AssertTest.java
+++ aula-06-assertivas/src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -3,6 +3,8 @@ package br.ce.wcaquino.servicos;
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.ce.wcaquino.entidades.Usuario;
+
 public class AssertTest {
 	
 	@Test
@@ -59,7 +61,29 @@ public class AssertTest {
 		
 		// Comparação de radical
 		Assert.assertTrue("bola".startsWith("bo"));
-
+		
+		//Comparação entre dois objetos
+		Usuario u1 = new Usuario("Usuario 1");
+		Usuario u2 = new Usuario("Usuario 1");
+		
+		/* 
+		 * Neste caso o assertEquals não considera esses dois objetos iguais, pois não 
+		 * existe um metodo equals implementado no objeto Usuario.
+		 * pois quem diz se um objeto é igual a outro objeto será o próprio objeto e para 
+		 * isso é necessario criar um metodo equals no objeto que esta sendo comparado, 
+		 * neste caso o objeto Usuario.
+		 */
+		/* Gera erro pois sem o metodo equals implementado em Usuario quem fará a comparação 
+		 * será o Objeto mais ancestral object e este fará a comparação entre instancias, isso 
+		 * retornará falso pois são instancia diferentes, embora sejam o mesmo objto
+		 */
+		//Assert.assertEquals(u1, u2); 
+		
+		// Desta forma o object mais ancestral de Usuario comparará a instancia e retornará verdadeiro
+		Assert.assertEquals(u1, u1); 
+		
+		
+		
 	}
 
 }
