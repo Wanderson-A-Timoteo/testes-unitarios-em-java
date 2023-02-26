489d99d2aa437d3aefa261f70ba8e9bad0ddd984
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-02-26 20:38:19 -0300

    Adicionado lib BuildMaster

--- aula-23-buildermaster/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ aula-23-buildermaster/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -32,6 +32,7 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
 import br.ce.wcaquino.exceptions.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
+import buildermaster.BuilderMaster;
 
 public class LocacaoServiceTest {
 
@@ -115,5 +116,9 @@ public class LocacaoServiceTest {
 		//verificacao
 		assertThat(retorno.getDataRetorno(), caiNumaSegunda());
 		
+	}
+	
+	public static void main(String[] args) {
+		new BuilderMaster().gerarCodigoClasse(Locacao.class);
 	}
 }
